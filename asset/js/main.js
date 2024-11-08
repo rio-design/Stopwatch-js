269d58f75e2b093215a71870885f0ea82cc19cfb
R-design359 <rio.design.2399@gmail.com>
2024-11-08 17:33:39 +0900

    ファイルを更新

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,11 +1,22 @@
 // alert('test');
 
+// カウントしている数字をディスプレイに表示
+let displayElm = document.getElementsByClassName('display')[0];
 // startButtonというclassが付いている要素のうち、
 // 最初のものを取り出す
 let startButton = document.getElementsByClassName('startButton')[0];
 
 // 取り出したstartButtonに対してクリックイベントのリスナを仕掛ける
-startButton.addEventListener('click', function() {
+startButton.addEventListener('click', function () {
     // この行はクリックしたときに呼ばれる
     console.log('start');
+
+    // スタートボタンをクリックでコンソールに数字をカウント
+    let seconds = 0;
+    setInterval(function () {
+        seconds++;
+        
+        displayElm.innerText = seconds;
+        console.log(seconds);
+    }, 1000);
 });
