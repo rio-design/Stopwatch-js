2462e6f7d7d6585aab04e0a6154ab5350d168f32
R-design359 <rio.design.2399@gmail.com>
2024-11-23 19:57:27 +0900

    Update file

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,4 +1,14 @@
+function addMessage(message) {
+    let messageElm = document.createElement('div');
+    let now = new Date();
+    messageElm.innerText = now.getHours() + '時' + now.getMinutes() + '分' + now.getSeconds() + '秒 ' + message;
+    messageElm.classList = ['message'];
+    logElm.appendChild(messageElm);
+}
+
+
 let displayElm = document.getElementsByClassName('display')[0];
+let logElm = document.querySelector('.log');
 let timer = null;
 let startButton = document.getElementsByClassName('startButton')[0];
 
@@ -10,6 +20,9 @@ startButton.addEventListener('click', function () {
             displayElm.innerText = seconds;
             console.log(seconds);
         }, 1000);
+
+        // スタートボタンを押す度に「開始」と表示
+        addMessage('開始');
     }
 });
 
@@ -18,5 +31,8 @@ stopButton.addEventListener('click', function () {
     if (timer !== null) {
         clearInterval(timer);
         timer = null;
+
+        // スタートボタンを押す度に「終了」と表示
+        addMessage('終了');
     }
 });
